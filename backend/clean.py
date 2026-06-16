6c4669f4e7b1b6c2b38261dd4714d73e1c0468ac
Temi <limiajayi@gmail.com>
2026-06-16 15:03:41 +0100

    Data cleaning done!

--- backend/clean.py
+++ backend/clean.py
@@ -19,10 +19,21 @@ def map_advantage(row):
     elif row["away_team"] == row["country"]:
         return 2
     
+def set_results(row):
+    if row["team_a_score"] > row["team_b_score"]:
+        return row["team_a_id"]
+    elif row["team_b_score"] > row["team_a_score"]:
+        return row["team_b_id"]
+    else:
+        return None
+    
+#filters results down to be inbetween the max date and min date    
 results = results[(results["date"] >= MIN_DATE) & (results["date"] <= MAX_DATE)].reset_index(drop=True)
 
+#filters results for the competitions defined above
 results = results[(results["tournament"].isin(COMPETIONS))].reset_index(drop=True)
 
+#sets a home advantage status. 0 = both teams away, 1 = team_a at home, 2 = team_b at home
 results["home_advantage_status"] = results.apply(map_advantage, axis=1)
 
 results = results.rename(columns={
@@ -30,17 +41,38 @@ results = results.rename(columns={
     "home_team": "team_a_name",
     "away_team": "team_b_name",
     "home_score": "team_a_score",
-    "away_score": "team_b_score"
+    "away_score": "team_b_score",
+    "country": "host_country"
 })
 
+#reads country.csv and maps it into a dictionary
 country = pd.read_csv("country.csv")
-
 name_to_id = {row["country_name"]: row["id"] for _, row in country.iterrows()}
 
+#sets ids for team a and team b to be used as a foreign key
 results["team_a_id"] = results["team_a_name"].map(name_to_id).astype("Int64")
 results["team_b_id"] = results["team_b_name"].map(name_to_id).astype("Int64")
-results = results.dropna(subset=["team_a_id", "team_b_id"]).reset_index()
 
-# print(name_to_id)
+#country ids and host country now match to be used as a foreign key as well
+results["host_country"] = results["host_country"].map(name_to_id).astype("Int64")
 
+#drops null values for these columns
+results = results.dropna(subset=["team_a_id", "team_b_id", "team_a_score", "team_b_score"]).reset_index()
+
+#sets these to whole numbers
+results["team_a_score"] = results["team_a_score"].astype("Int64")
+results["team_b_score"] = results["team_b_score"].astype("Int64")
+
+#create a new column called match_results that has the results for eeeeverything
+# -1 == draw, anything else is an existing country
+results["match_results"] = results.apply(set_results, axis=1)
+results["match_results"] = results["match_results"].astype("Int64")
+
+#add an id field to be index + 1
+results["id"] = results.index + 1
+
+#drop unnecessary columns
+results = results.drop(columns=["city", "neutral", "index", "team_a_name", "team_b_name"])
+
+results.to_csv("matches.csv", index=False)
 print(results)
